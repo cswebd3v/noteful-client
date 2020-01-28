be056bdd8e4fc97c24eac3f3be32ac1eab7107c3
Chris Swenson <cswebd3v@gmail.com>
2020-01-28 17:28:36 -0600

    Updates

---
+++ precache-manifest.fb3f31e92ef5059b95fe2ca4ed5545ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fee81f5920dc17cbaab112231d501d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d041c0ca46416e333035",
+    "url": "/static/css/main.ead198c0.chunk.css"
+  },
+  {
+    "revision": "c84881d90b373dd76e6b",
+    "url": "/static/js/2.d6ba497a.chunk.js"
+  },
+  {
+    "revision": "2fcfb7b1dd14bf9d69b9b9e679cf7d0c",
+    "url": "/static/js/2.d6ba497a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d041c0ca46416e333035",
+    "url": "/static/js/main.5914b363.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
